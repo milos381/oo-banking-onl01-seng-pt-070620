e0b84b45e230e3684e5a58d19e42a91a898ecb84
Milos Popovic <milos_popovic@outlook.com>
2020-07-30 21:06:18 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,21 +20,35 @@ class Transfer
 
   def execute_transaction
     #binding.pry
-    if @sender.balance < @amount || @sender.status == "closed"
+    # if @sender.balance < @amount || @sender.status == "closed"
+    #   @status = "rejected"
+    #   return "Transaction rejected. Please check your account balance."
+    #
+    # elsif @status == "complete"
+    #   return "Transaction was already excuted"
+    # else
+    #   @sender.deposit( @amount * -1)
+    #   @receiver.deposit( @amount )
+    #   @status = "complete"
+    # end
+    if @sender.balance >= @amount && valid? && @status == "pending"
+         @sender.deposit( @amount * -1)
+         @receiver.deposit( @amount )
+         @status = "complete"
+
+    else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.balance = @sender.balance - @amount
-      @receiver.deposit( @amount )
-      @status = "complete"
     end
   end
 
   def reverse_transfer
-
+    if @status == "complete"
+        @sender.deposit( @amount )
+        @receiver.deposit( @amount * -1)
+        @status = "reversed"
+    end
   end
 
 
