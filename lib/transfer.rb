7c5cab1af3b69e8a152f0ab3067ba5fe320a1fc8
Milos Popovic <milos_popovic@outlook.com>
2020-07-30 20:57:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,9 +23,6 @@ class Transfer
     if @sender.balance < @amount || @sender.status == "closed"
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      return "Transaction was already excuted"
     else
       @sender.deposit( @amount * -1)
       @receiver.deposit( @amount )
